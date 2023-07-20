feebe155eac9ba61f9c5e6aac06cb8ab675aaf44
Parker Ellyn Madlock <parker.madlock@gmail.com>
2023-07-20 14:32:51 -0500

    Svelte Index Page

---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+<!-- src/routes/index.svelte -->
+<script>
+    // Add any script logic here
+  </script>
+  
+  <main>
+    <h1>Hello, SvelteKit!</h1>
+  </main>
+  
+  <style>
+    /* Add any CSS styles here */
+  </style>
